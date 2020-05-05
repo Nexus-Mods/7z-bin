3c66b6d2e169c5b081d65b4d4c4a4b2e04f641ab
TanninOne <sherb@gmx.net>
2020-05-05 16:02:03 +0200

    added linux support

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var exeName = {
   win32: '7z.exe'
 }[process.platform] || '7z';
 
-module.exports = path.join(__dirname, 'win', exeName);
+module.exports = path.join(__dirname, process.platform, exeName);
