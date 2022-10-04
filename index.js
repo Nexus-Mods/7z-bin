018d9229edf5ae962ede43a2ef30a68bc739d42c
TanninOne <seppleviathan@gmx.de>
2022-10-04 11:15:50 +0200

    updated 7z to version 22.0.1

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 var path = require('path');
 
 var exeName = {
-  win32: '7z.exe'
+  win32: '7z.exe',
+  linux: '7zzs',
 }[process.platform] || '7z';
 
 module.exports = path.join(__dirname, process.platform, exeName);
